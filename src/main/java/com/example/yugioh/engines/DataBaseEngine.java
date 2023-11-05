db56bb9be919929e00f15fead571758598a6611c
Lyna <djelmoudi.lyna@gmail.com>
2023-11-05 20:15:19 +0100

    Refactoring some code and enhancement of deckBuilder interface in the searching card component.

--- src/main/java/com/example/yugioh/engines/DataBaseEngine.java
+++ src/main/java/com/example/yugioh/engines/DataBaseEngine.java
@@ -68,7 +68,7 @@ public class DataBaseEngine {
                         + "database=yugioh;"
                         + "integratedSecurity=false;"
                         + "user=Lola;"
-                        + "password=test1234;"
+                        + "password=Test1234;"
                         + "encrypt=false;"
                         + "sendStringParametersAsUnicode=true;"
                         + "useBulkCopyForBatchInsert=true;"
