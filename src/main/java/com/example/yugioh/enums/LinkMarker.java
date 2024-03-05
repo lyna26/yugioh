04c3bab45fff70422efb6df4d69449b79e8ae387
Lyna <40266991+lyna26@users.noreply.github.com>
2024-03-05 21:29:33 +0100

    deckBuilder correction

--- src/main/java/com/example/yugioh/enums/LinkMarker.java
+++ src/main/java/com/example/yugioh/enums/LinkMarker.java
@@ -2,13 +2,13 @@ package com.example.yugioh.enums;
 
 public enum LinkMarker {
     BOTTOM("\u2193"), // Flèche vers le bas Unicode
-    BOTTOM_LEFT("\u2199"), // Flèche vers le bas à gauche Unicode
-    BOTTOM_RIGHT("\u2198"), // Flèche vers le bas à droite Unicode
-    TOP("\u2191"),   // Flèche vers le haut Unicode
-    TOP_LEFT("\u2196"), // Flèche vers le haut à gauche Unicode
-    TOP_RIGHT("\u2197"), // Flèche vers le haut à droite Unicode
-    LEFT("\u2190"), // Flèche vers la gauche Unicode
-    RIGHT("\u2192"); // Flèche vers la droite Unicode
+    BOTTOM_LEFT("\u2199"),
+    BOTTOM_RIGHT("\u2198"),
+    TOP("\u2191"),
+    TOP_LEFT("\u2196"),
+    TOP_RIGHT("\u2197"),
+    LEFT("\u2190"),
+    RIGHT("\u2192");
 
     private final String arrow;
 
@@ -26,6 +26,6 @@ public enum LinkMarker {
                 return linkMarker.getArrow();
             }
         }
-        return marker; // Retourne le mot original s'il n'est pas trouvé dans l'enum
+        return marker;
     }
 }
