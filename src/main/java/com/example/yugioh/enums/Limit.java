15a24730498746d92726f133ae2b4f15efa826e1
Lyna <40266991+lyna26@users.noreply.github.com>
2024-11-10 04:16:11 +0100

    deckBuilder correction

--- src/main/java/com/example/yugioh/enums/Limit.java
+++ src/main/java/com/example/yugioh/enums/Limit.java
@@ -7,8 +7,8 @@ public enum Limit {
 
     FORBIDDEN(0, "FORBIDDEN"),
     LIMITED(1, "LIMITED"),
-    SEMI_LIMITED(2, "SEMI-LIMITED"),
-    NO_LIMITED(3, "NOT-LIMITED");
+    SEMI_LIMITED(2, "SEMI_LIMITED"),
+    NO_LIMITED(3, "NO_LIMITED");
 
     private final int nbCopies;
 
